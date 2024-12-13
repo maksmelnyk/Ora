25e0e65a8b3b8b45b19f6c74c47be2923acded5e
MaxM <109100100+maksmelnyk@users.noreply.github.com>
2024-12-13 19:59:50 +0200

    Add SecurityConfig to Profile service

--- auth/src/main/java/com/example/auth/AuthApplication.java
+++ auth/src/main/java/com/example/auth/AuthApplication.java
@@ -1,11 +1,13 @@
 package com.example.auth;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class AuthApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
 }
--- auth/src/main/java/com/example/auth/SecurityConfig.java
+++ auth/src/main/java/com/example/auth/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         a -> a.requestMatchers("/api/v1/auth/register").permitAll().anyRequest().authenticated());
 
