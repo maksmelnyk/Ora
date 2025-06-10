139d6b9711f5981c0f934a528b0f511e5ca11444
MaxM <109100100+maksmelnyk@users.noreply.github.com>
2025-06-10 12:12:45 +0300

    refactor(profile-service): update init_migration script

--- backend/profile/migrations/changelog/20250101010101_init_migration.sql
+++ backend/profile/migrations/changelog/20250101010101_init_migration.sql
@@ -2,10 +2,11 @@ begin;
 
 create table if not exists user_profile (
     id                  uuid            not null    primary key,
+    username            varchar(50)     not null,
     first_name          varchar(50)     not null,
     last_name           varchar(50)     not null,
-    image_url           varchar(500),
-    bio                 varchar(2000),
+    image_url           varchar(200),
+    bio                 varchar(200),
     birth_date          date            not null,
     created_date        timestamptz     not null,
     last_modified_date  timestamptz,
@@ -20,6 +21,7 @@ create table if not exists educator_profile (
     user_profile_id     uuid            not null    references user_profile(id),
     bio                 varchar(2000),
     experience          varchar(2000),
+    video_url           varchar(200),
     status              int             not null,
     has_product         boolean         not null,
     created_date        timestamptz     not null,
