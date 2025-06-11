d4037dee91d8221f910498df98ee74c44c28f882
MaxM <109100100+maksmelnyk@users.noreply.github.com>
2025-06-11 15:33:41 +0300

    refactor(web): update getEnv function

--- frontend/web/src/common/configs/env.ts
+++ frontend/web/src/common/configs/env.ts
@@ -6,12 +6,15 @@ const getEnv = (): EnvType => {
   if (typeof window !== "undefined" && (window as any)._env_) {
     return (window as any)._env_;
   }
-  if (typeof process !== "undefined" && process.env) {
-    return process.env;
-  }
+
   if (typeof import.meta !== "undefined" && import.meta.env) {
     return import.meta.env as EnvType;
   }
+
+  if (typeof process !== "undefined" && process.env) {
+    return process.env;
+  }
+
   return {};
 };
 
